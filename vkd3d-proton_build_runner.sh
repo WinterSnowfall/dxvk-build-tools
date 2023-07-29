1c2917563e7c2abb466e02af914a40a60062bab2
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2023-07-29 03:07:53 +0300

    Simplify vkd3d-proton runner

--- vkd3d-proton_build_runner.sh
+++ vkd3d-proton_build_runner.sh
@@ -8,20 +8,15 @@ TIMEZONE=Etc/UTC
 
 if [ $# -ge 1 ]
 then
-    if [ $# -ge 2 ]
-    then
-        BUILD_NAME=$2
-    fi
-    docker run -ti --rm \
-               --name vkd3d-proton-builder \
-               -h vkd3d-proton-builder  \
-               -e TZ="$TIMEZONE" \
-               -e PROJECT_NAME="$1" \
-               -e BUILD_NAME="$BUILD_NAME" \
-               -v $SOURCE_PATH:/home/builder/source \
-               -v $OUTPUT_PATH:/home/builder/output \
-               archlinux/vkd3d-proton
-else
-    echo "Please specify the project name!"
+    BUILD_NAME=$2
 fi
+docker run -ti --rm \
+           --name vkd3d-proton-builder \
+           -h vkd3d-proton-builder  \
+           -e TZ="$TIMEZONE" \
+           -e PROJECT_NAME=vkd3d-proton \
+           -e BUILD_NAME="$BUILD_NAME" \
+           -v $SOURCE_PATH:/home/builder/source \
+           -v $OUTPUT_PATH:/home/builder/output \
+           archlinux/vkd3d-proton
 
