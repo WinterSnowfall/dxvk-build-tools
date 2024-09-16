dbe0cb783f81493e20a0849e427f8a5c5f9e0852
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2024-09-16 10:56:05 +0300

    Add the option to run the docker builder remotely via ssh

--- repo_build_runner.sh
+++ repo_build_runner.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# build locally or trigger remote builds via ssh
+LOCAL_BUILD=true
+REMOTE_HOST="ssh://127.0.0.1"
+# relative paths that need to be absolute when building remotely
 SOURCE_PATH="$PWD/source"
 OUTPUT_PATH="$PWD/output"
 MISC_PATH="$PWD/misc"
@@ -16,16 +20,30 @@ then
         BUILD_NAME="$2"
     fi
 
-    docker run -ti --rm \
-               --name dxvk-builder \
-               -h dxvk-builder \
-               -e TZ="$TIMEZONE" \
-               -e REPO_NAME="$REPO_NAME" \
-               -e BUILD_NAME="$BUILD_NAME" \
-               -v "$SOURCE_PATH":/home/builder/source \
-               -v "$OUTPUT_PATH":/home/builder/output \
-               -v "$MISC_PATH":/home/builder/misc \
-               dxvk-builder
+    if $LOCAL_BUILD
+    then
+        docker run -ti --rm \
+                   --name dxvk-builder \
+                   -h dxvk-builder \
+                   -e TZ="$TIMEZONE" \
+                   -e REPO_NAME="$REPO_NAME" \
+                   -e BUILD_NAME="$BUILD_NAME" \
+                   -v "$SOURCE_PATH":/home/builder/source \
+                   -v "$OUTPUT_PATH":/home/builder/output \
+                   -v "$MISC_PATH":/home/builder/misc \
+                   dxvk-builder
+    else
+        docker -H "$REMOTE_HOST" run -ti --rm \
+                   --name dxvk-builder \
+                   -h dxvk-builder \
+                   -e TZ="$TIMEZONE" \
+                   -e REPO_NAME="$REPO_NAME" \
+                   -e BUILD_NAME="$BUILD_NAME" \
+                   -v "$SOURCE_PATH":/home/builder/source \
+                   -v "$OUTPUT_PATH":/home/builder/output \
+                   -v "$MISC_PATH":/home/builder/misc \
+                   dxvk-builder
+    fi
 else
     echo "Please specify the repository name!"
     exit 1
